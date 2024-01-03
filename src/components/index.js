e688fef11d32cae10d237a2127f2732c2e464373
AtamanKit <rodionatamaniuc@gmail.com>
2024-01-03 18:13:18 +0200

    Special Offer

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import Button from "./Button";
+import ShoeCard from "./ShoeCard";
+import PopularProductCard from "./PopularProductCard";
+import ServiceCard from "./ServiceCard";
+
+
+export {
+    Button,
+    ShoeCard,
+    PopularProductCard,
+    ServiceCard,
+
+
+}
