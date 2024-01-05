f343ec35a6ba6a1b6cd20e4a5018c393f11d8e9e
AtamanKit <rodionatamaniuc@gmail.com>
2024-01-05 07:32:50 +0200

    footer created

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ const App = () => (
     <section className="bg-pale-blue padding">
       <CustomerReviews />
     </section>
-    <section className="padding-x">
+    <section className="padding-x sm:py-32 py-16 w-full">
       <Subscribe />
     </section>
     <section className="bg-black padding-x padding-t pb-8">
--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ import Button from "./Button";
 import ShoeCard from "./ShoeCard";
 import PopularProductCard from "./PopularProductCard";
 import ServiceCard from "./ServiceCard";
+import ReviewCard from "./ReviewCard";
 
 
 export {
@@ -9,6 +10,8 @@ export {
     ShoeCard,
     PopularProductCard,
     ServiceCard,
+    ReviewCard,
+
 
 
 }
